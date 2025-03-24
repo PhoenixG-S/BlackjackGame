ed33529c93a5bce8a3e28e8977efa498774830fe
raghav2704 <rbgamer224@gmail.com>
2025-03-24 20:12:51 +1300

    Update BlackjackGame.java
    need to add new class

--- src/pdc_assignment/BlackjackGame.java
+++ src/pdc_assignment/BlackjackGame.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author phoen
+ * @author raghav
  */
 
 public class BlackjackGame {
@@ -17,22 +17,28 @@ public class BlackjackGame {
     private Player player;
     private Dealer dealer;
     private CheckInput checkInput;
+    private BettingSystem bettingSystem;
 
-    public BlackjackGame(String playerName) {
-        deck = new Deck();  
+    public BlackjackGame(String playerName, double initialBalance) {
+        deck = new Deck();
         player = new Player(playerName);  
-        dealer = new Dealer();  
-        checkInput = new CheckInput();  
+        dealer = new Dealer();
+        checkInput = new CheckInput();
+        bettingSystem = new BettingSystem(10.0, initialBalance); 
     }
 
     public void playGame() {
-
         while (true) {
-            
             resetGame();
-            
-            System.out.println("Welcome to Blackjack, " + player.getName() + "!\n");
 
+            System.out.println("Welcome to Blackjack, " + player.getName() + "!\n");
+            System.out.println("Your current betting balance: $" + bettingSystem.getPlayerBalance());
+
+      
+            double betAmount = getBetAmount();
+            bettingSystem.placeBet(betAmount);  
+            
+   
             player.addCard(deck.dealCard());
             player.addCard(deck.dealCard());
             dealer.addCard(deck.dealCard());
@@ -41,50 +47,74 @@ public class BlackjackGame {
             System.out.println(player);
             dealer.showFirstCard();
 
-            if (!playerTurn()) { 
-                System.out.println("You busted! Dealer wins.");
-                if (!askToPlayAgain()) break;
-                continue;
-            }
-            
-            dealerTurn();
+            playerTurn();
 
-            determineWinner();
+            if (player.calculateHandScore() > 21) {
+                System.out.println("You busted! Dealer wins.");
+                bettingSystem.payout(false);  
+            } else {
+                dealerTurn();
+                determineWinner();
+            }
 
             deck.reShuffle();
 
             if (!askToPlayAgain()) {
-                System.out.println("Thanks for playing!");
+                System.out.println("Thanks for playing! Final balance: $" + bettingSystem.getPlayerBalance());
                 break;
             }
         }
     }
-    
-    private void resetGame() {
-        player.clearHand();
-        dealer.clearHand();  
-        deck.reShuffle();    
+
+    private double getBetAmount() {
+        double betAmount = 0;
+        while (true) {
+            System.out.println("Enter your bet: ");
+            betAmount = checkInput.getBetAmount("");
+            if (betAmount <= 0) {
+                System.out.println("Bet must be greater than 0.");
+            } else if (betAmount > bettingSystem.getPlayerBalance()) {
+                System.out.println("You don't have enough balance to place that bet.");
+            } else {
+                break;
+            }
+        }
+        return betAmount;
     }
 
-    private boolean playerTurn() {
-        while (true) {
+    private void resetGame() {
+        player.clearHand();
+        dealer.clearHand();
+        deck.reShuffle();
+    }
+
+    private void playerTurn() {
+        while (player.calculateHandScore() < 21) {
             CheckInput.Choice choice = checkInput.getChoice("Hit (H) or Stand (S)? ");
+            
             if (choice == CheckInput.Choice.H) {
                 player.addCard(deck.dealCard());
                 System.out.println(player);
-                if (player.isBusted()) {
-                    return false;
-                }
             } else {
-                return true;
+                break;
             }
+        }
+
+        if (player.calculateHandScore() > 21) {
+            System.out.println("You busted! Dealer wins.");
+            bettingSystem.payout(false);  
         }
     }
 
     private void dealerTurn() {
-    System.out.println("\nDealer reveals their hand:");
-    dealer.playTurn(deck);
-}
+        System.out.println("\nDealer reveals their hand:");
+        System.out.println(dealer);
+
+        while (dealer.calculateHandScore() < 17) {
+            dealer.addCard(deck.dealCard());
+            System.out.println(dealer);
+        }
+    }
 
     private void determineWinner() {
         int playerScore = player.calculateHandScore();
@@ -92,15 +122,17 @@ public class BlackjackGame {
 
         if (dealerScore > 21 || playerScore > dealerScore) {
             System.out.println("Congratulations! You win!");
+            bettingSystem.payout(true);  
         } else if (playerScore < dealerScore) {
             System.out.println("Dealer wins. Better luck next time.");
+            bettingSystem.payout(false);  
         } else {
             System.out.println("It's a tie!");
         }
     }
 
     private boolean askToPlayAgain() {
-        return checkInput.playAgainResponse("Do you want to play again? ('Y' for Yes or 'N' for No): ");
+        return checkInput.playAgainResponse("Do you want to play again? (Y/N): ");
     }
 
     public static void main(String[] args) {
@@ -108,7 +140,12 @@ public class BlackjackGame {
         System.out.print("Enter your name: ");
         String playerName = scanner.nextLine();
 
-        BlackjackGame game = new BlackjackGame(playerName);
+        System.out.print("Enter the amount you have: ");
+        double initialBalance = scanner.nextDouble();
+
+        BlackjackGame game = new BlackjackGame(playerName, initialBalance);
         game.playGame();
+        scanner.close();
     }
 }
+
